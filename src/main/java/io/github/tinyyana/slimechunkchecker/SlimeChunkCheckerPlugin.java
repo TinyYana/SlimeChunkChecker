61e5f75c66852b9f0fc19ac15519f064993f4b25
TinyYana <yanasakuranight@gmail.com>
2021-10-30 16:11:16 +0800

    add config loader

--- src/main/java/io/github/tinyyana/slimechunkchecker/SlimeChunkCheckerPlugin.java
+++ src/main/java/io/github/tinyyana/slimechunkchecker/SlimeChunkCheckerPlugin.java
@@ -1,25 +1,33 @@
 package io.github.tinyyana.slimechunkchecker;
 
-import org.bukkit.configuration.file.FileConfiguration;
+import io.github.tinyyana.slimechunkchecker.Commands.ReloadConfig;
+import io.github.tinyyana.slimechunkchecker.Commands.ChunkCheck;
+import io.github.tinyyana.slimechunkchecker.Utils.ConfigLoader;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SlimeChunkCheckerPlugin extends JavaPlugin {
 
-    FileConfiguration config = getConfig();
+    public static ConfigLoader config;
+    public static SlimeChunkCheckerPlugin plugin;
 
     public void onEnable() {
-        loadConfig();
-        getCommand("slime").setExecutor(new SlimeChunkCheckCommand(this));
-        getCommand("slimereload").setExecutor(new ReloadConfigCommand(this));
+        config = new ConfigLoader("config");
+        getCommand("slime").setExecutor(new ChunkCheck(this));
+        getCommand("slimereload").setExecutor(new ReloadConfig(this));
     }
 
     public void onDisable() {
     }
 
-    public void loadConfig() {
-        config.options().copyDefaults(true);
-        this.saveDefaultConfig();
-        this.getConfig();
-        saveConfig();
+    public void onLoad() {
+        plugin = this;
+    }
+
+    public static SlimeChunkCheckerPlugin getInstance() {
+        return SlimeChunkCheckerPlugin.plugin;
+    }
+
+    public static SlimeChunkCheckerPlugin getPlugin() {
+        return plugin;
     }
 }
